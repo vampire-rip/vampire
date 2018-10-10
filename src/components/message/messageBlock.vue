8f195c6f2d6c71fbe9013e69bba371dee4bf18d8
SunriseFox <sunrisefox@qq.com>
2018-10-10 15:21:28 +0800

    Add message(//TODO: rename to notice)

--- src/components/message/messageBlock.vue
+++ src/components/message/messageBlock.vue
@@ -1,48 +1,54 @@
 <script>
-  import { mapState, mapMutations } from 'vuex'
-  export default {
-    name: 'message-block',
-    data() {
-      return {
+import {mapState, mapMutations} from 'vuex';
 
-      }
-    },
-    beforeMount() {
+export default {
+  name: 'message-block',
+  data() {
+    return {};
+  },
+  props: ['id', 'source', 'content'],
+  beforeMount() {
 
-    },
-    mounted() {
+  },
+  mounted() {
 
-    },
-    destroyed() {
+  },
+  destroyed() {
 
-    },
-    computed: {
-      ...mapState({
-
-      })
-    },
-    methods: {
-      ...mapMutations({
-
-      })
-    },
-  };
+  },
+  computed: {
+    ...mapState({}),
+  },
+  methods: {
+    ...mapMutations({}),
+  },
+};
 
 </script>
 <!-- vue-loader's scoped css won't work with style-loader -->
-<style scoped lang="vcss">
+<style scoped lang="scss">
 .message-block {
-  display: block;
+  pointer-events: all;
+  display: flex;
+  position: relative;
   flex: 0 1 auto;
-  height: 100px;
-  background: goldenrod;
+  min-height: 80px;
   padding: 10px 10px;
   margin: 10px 10px;
+  border-width: 0 0 0 5px;
+  border-style: solid;
+  border-radius: 4px;
+  background: #BDE2ED;
+  border-color: #5BC0EB;
+  color: #115977;
+  justify-content: space-around;
+  flex-direction: column;
 }
 </style>
 
 <template>
-  <div :id="$options.name" :class="$options.name">
-
-  </div>
+<div :id="$options.name" :class="$options.name">
+  <div style="display: flex"><b>来自 {{source}} 的消息：</b><div class="delete" style="margin-left: auto"></div></div>
+  <div class="content">{{content}}</div>
+</div>
 </template>
