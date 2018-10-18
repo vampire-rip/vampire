bf2587d747c67bb32c761b687b716a6f477a4ffb
SunriseFox <sunrisefox@qq.com>
2018-10-18 10:13:27 +0800

    Added startup page loader...

--- src/entry.js
+++ src/entry.js
@@ -12,7 +12,9 @@ if ('serviceWorker' in navigator) {
       catch((error) => {
         console.warn('↓ While loading script, expecting [Module], received [Error]');
         console.error(error);
-        alert('脚本下载失败了T^T 刷新试试？')
+        document.querySelector('#loader-text').innerHTML =
+            '<span>脚本下载失败了T^T 刷新试试？</span>' +
+            '<pre>'+ error.stack +'</pre>'
       }).
       then(() => {
         navigator.serviceWorker.addEventListener('message', event => {
@@ -22,9 +24,19 @@ if ('serviceWorker' in navigator) {
             window.postMessage({type: 'notice', source:'Service Worker', content: '网络连接失败，错误：' + event.data.error, error: true}, '*');
           }
         });
-        document.querySelector('#loading').remove();
+        document.querySelector('#page-loader').remove();
+        document.body.classList.remove('has-loader');
         navigator.serviceWorker.controller.postMessage({type: 'ready'});
       });
 } else {
-  import('./index');
+  try {
+    import('./index');
+    document.querySelector('#page-loader').remove();
+    document.body.classList.remove('has-loader');
+  } catch (error) {
+    console.error(error);
+    document.querySelector('#loader-text').innerHTML =
+        '<span>不支持您的浏览器，请升级QwQ</span>' +
+        '<pre>'+ error.stack +'</pre>'
+  }
 }
